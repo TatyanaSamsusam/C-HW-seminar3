b178a4ac6f7a1811060756c68eedc25f95fabea4
Tatyana <android87@list.ru>
2022-05-21 15:48:58 +0400

    tablitsa kubov

---
+++ task03_tablitsaKubov/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.WriteLine("введите число, и я выдам таблицу кубов от 1 до вашего числа: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"вот таблица кубов от 1 до {number}:");
+
+for (int i = 1; i <= number; i++)
+{
+    for(int j = 1; j <= number; j++)
+    {
+        Console.WriteLine($"{j * j * j}");
+    }
+    break;
+}
